917d2b0dd8c56d9e24dd00acf4cf639167c9af07
Jean-Francois Pombert <jpombert@iit.edu>
2016-05-31 07:01:03 -0500

    Added support for various formats

--- Misc_tools/queryNuccore.pl
+++ Misc_tools/queryNuccore.pl
@@ -1,30 +1,94 @@
 #!/usr/bin/perl
 ## Pombert JF,  Illinois Tech 2016
-## Retrieve fasta files from the NCBI Nucleotide database using a list of accession numbers (one per line)
+## Retrieve genomes/proteins from the NCBI Nucleotide database using a list of accession numbers (one per line)
 
-use LWP::Simple;
 use strict;
 use warnings;
+use Getopt::Long qw(GetOptions);
+use LWP::Simple;
 
-my $usage = "USAGE = queryNuccore.pl Acession_list";
+my $usage = "
+USAGE = queryNuccore.pl [options]
+
+EXAMPLE: queryNuccore.pl -fa -gbk -l accession.txt
+
+OPTIONS:
+-fa (--fasta)	Reference genome(s) in fasta format
+-gb (--genbank)	Reference genome(s) in GenBank format
+-sqn (--sequin)	Reference genome(s) in Sequin ASN format
+-p (--proteins)	Protein sequences (amino acids)
+-c (--cds)	Protein sequences (nucleotides)
+-l (--list)	Accession numbers list, one accession per line
+";
 die "$usage\n" unless@ARGV;
+
+## Options
+my $fasta;
+my $genbank;
+my $proteins;
+my $sequin;
+my $cds;
+my $list = '';
+
+GetOptions(
+	'fasta|fa' => \$fasta,
+	'genbank|gb' => \$genbank,
+	'proteins|p' => \$proteins,
+	'sequin|sqn' => \$sequin,
+	'cds|c' => \$cds,
+	'list|l=s' => \$list,
+);
 
 my $start = localtime();
 my $tstart = time;
 
-open IN, "<$ARGV[0]";
+open IN, "<$list";
 
-my $url1 = 'http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nuccore&id=';
-my $url2 = '&rettype=fasta&retmode=text';
+my $efetch = 'http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi';
+my $rettype = undef;
+my $retmode = undef;
+my $db = 'nuccore';
 
 while (my $accession = <IN>){
 	chomp $accession;
 	if ($accession =~ /^#/){next;} ## Discarding comments
-	else{
-		my $filename = "$accession".'.fasta';
-		my $URL = $url1.$accession.$url2;
-		system "echo Downloading $accession.fasta ...";
-		getstore($URL, $filename);
+	if ($fasta || $genbank || $proteins || $sequin || $cds){
+		$retmode = 'text';
+		if ($fasta){
+			$rettype ='fasta';
+			my $name = "$accession".'.fasta';
+			my $URL = $efetch.'?db='.$db.'&id='.$accession.'&rettype='.$rettype.'&retmode='.$retmode;
+			system "echo Downloading $accession.fasta ...";
+			getstore($URL, $name);
+		}
+		if ($genbank){
+			$rettype ='gbwithparts';
+			my $name = "$accession".'.gbk';
+			my $URL = $efetch.'?db='.$db.'&id='.$accession.'&rettype='.$rettype.'&retmode='.$retmode;
+			system "echo Downloading $accession.gbk ...";
+			getstore($URL, $name);
+		}
+		if ($sequin){
+			$rettype ='';
+			my $name = "$accession".'.sqn';
+			my $URL = $efetch.'?db='.$db.'&id='.$accession.'&rettype='.$rettype.'&retmode='.$retmode;
+			system "echo Downloading $accession.sqn ...";
+			getstore($URL, $name);
+		}
+		if ($proteins){
+			$rettype ='fasta_cds_na';
+			my $name = "$accession".'.fna';
+			my $URL = $efetch.'?db='.$db.'&id='.$accession.'&rettype='.$rettype.'&retmode='.$retmode;
+			system "echo Downloading $accession.fna ...";
+			getstore($URL, $name);
+		}
+		if ($cds){
+			$rettype ='fasta_cds_aa';
+			my $name = "$accession".'.faa';
+			my $URL = $efetch.'?db='.$db.'&id='.$accession.'&rettype='.$rettype.'&retmode='.$retmode;
+			system "echo Downloading $accession.faa ...";
+			getstore($URL, $name);
+		}
 	}
 }
 
