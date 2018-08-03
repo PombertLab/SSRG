4ce172b548fc8e92b86a977a0c443052ad484fdb
Jean-Francois Pombert <jpombert@iit.edu>
2018-08-03 13:53:12 -0500

    new read-mapping only options
    - Added option to perform only the read-mapping and skip the variant calling
    
    -Added an option to skip the stats calculations (those can take a while with large eukaryote genomes)
    
    - Fixed bug preventing pipeline to continue if encountering an empty BAM files (if no read mappped to the reference)
    
    - Fixed potential division by zero issue if sequencing depth = 0 if no read mapped to the reference

--- SSRG/get_SNPs.pl
+++ SSRG/get_SNPs.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl
 ## Pombert JF, Illinois Tech - 2018
-my $version = '1.8';
+my $version = '1.9';
 
 use strict; use warnings; use File::Basename; use Getopt::Long qw(GetOptions);
 
@@ -23,6 +23,7 @@ USAGE = perl get_SNPs.pl [options]
 EXAMPLE (simple): get_SNPs.pl -fa *.fasta -fq *.fastq
 EXAMPLE (advanced): get_SNPs.pl --fasta *.fasta --fastq *.fastq --mapper bowtie2 --caller varscan2 --type both --var ./VarScan.v2.4.3.jar --threads 16
 EXAMPLE (paired ends): get_SNPs.pl --fasta *.fasta --pe1 *R1.fastq --pe2 *R2.fastq --X 1000 --mapper bowtie2 --caller freebayes --threads 16
+EXAMPLE (read-mapping only): get_SNPs.pl --fasta *.fasta --pe1 *R1.fastq --pe2 *R2.fastq --X 1000 --mapper bowtie2 --rmo --bam --threads 16
 EXAMPLE (MASH): get_SNPs.pl -fa *.fasta -out Mash.txt -sort
 USAGE
 die "$usage\n$hint\n" unless@ARGV;
@@ -34,16 +35,18 @@ OPTIONS:
 -v (--version)	Display script version
 
 ### Mapping options ###
--fa (--fasta)	Reference genome(s) in fasta file
--fq (--fastq)	Fastq reads (single ends) to be mapped against reference(s)
--pe1		Fastq reads #1 (paired ends) to be mapped against reference(s)
--pe2		Fastq reads #2 (paired ends) to be mapped against reference(s)
--X		Maximum paired ends insert size (for bowtie2) [default: 750]
--mapper		Read mapping tool: bwa, bowtie2 or hisat2 [default: bowtie2]
--algo		BWA mapping algorithm:  bwasw, mem, samse [default: bwasw]
--threads	Number of processing threads [default: 16]
--bam		Keeps BAM files generated
--sam		Keeps SAM files generated; SAM files can be quite large
+-fa (--fasta)			Reference genome(s) in fasta file
+-fq (--fastq)			Fastq reads (single ends) to be mapped against reference(s)
+-pe1				Fastq reads #1 (paired ends) to be mapped against reference(s)
+-pe2				Fastq reads #2 (paired ends) to be mapped against reference(s)
+-X				Maximum paired ends insert size (for bowtie2) [default: 750]
+-mapper				Read mapping tool: bwa, bowtie2 or hisat2 [default: bowtie2]
+-algo				BWA mapping algorithm:  bwasw, mem, samse [default: bwasw]
+-threads			Number of processing threads [default: 16]
+-bam				Keeps BAM files generated
+-sam				Keeps SAM files generated; SAM files can be quite large
+-rmo (--read_mapping_only)	Do not perform variant calling; useful when only interested in bam/sam files and/or mapping stats
+-ns (--no_stats)		Do not calculate stats; stats can take a while to compute for large eukaryote genomes
 
 ### Variant caller options ###
 -caller				[default: varscan2]	## Variant caller: varscan2, bcftools or freebayes
@@ -76,6 +79,8 @@ my $sam = ''; my $bam = '';
 my @fasta; my @fastq;
 my @pe1; my @pe2;
 my $maxins = '750';
+my $rmo;
+my $nostat;
 ## Variant caller
 my $caller = 'varscan2';
 my $type = 'snp';
@@ -105,6 +110,8 @@ GetOptions(
 	'fa|fasta=s@{1,}' => \@fasta, 'fq|fastq=s@{1,}' => \@fastq,
 	'pe1=s@{1,}' => \@pe1, 'pe2=s@{1,}' => \@pe2,
 	'X=i' => \$maxins,
+	'rmo|read_mapping_only' => \$rmo,
+	'ns|no_stats' => \$nostat,
 	## Variant caller
 	'caller=s' => \$caller, 'type=s' => \$type, 'ploidy=i' => \$ploidy,
 	## VarScan-specific
@@ -121,6 +128,14 @@ GetOptions(
 );
 
 if ($help){die "$usage\n$options";} if ($vn){die "\nversion $version\n\n";}
+
+## Program checks
+my $prog = `command -v ${samtools}samtools`; chomp $prog; if ($prog eq ''){print "\nERROR: Cannot find SAMtools. Please install SAMtools in your path or include its installation directory at the top of get_SNPs.pl\n\n"; exit;}
+if ($mapper eq 'bowtie2'){my $prog = `command -v ${bowtie2}bowtie2`; chomp $prog; if ($prog eq ''){print "\nERROR: Cannot find Bowtie2. Please install Bowtie2 in your path or include its installation directory at the top of get_SNPs.pl\n\n"; exit;}}
+if ($mapper eq 'bwa'){my $prog = `command -v ${bwa}bwa`; chomp $prog; if ($prog eq ''){print "\nERROR: Cannot find BWA. Please install BWA in your path or include its installation directory at the top of get_SNPs.pl\n\n"; exit;}}
+if ($mapper eq 'hisat2'){my $prog = `command -v ${hisat2}hisat2`; chomp $prog; if ($prog eq ''){print "\nERROR: Cannot find HISAT2. Please install HISAT2 in your path or include its installation directory at the top of get_SNPs.pl\n\n"; exit;}}
+if ($caller eq 'freebayes'){my $prog = `command -v ${freebayes}freebayes`; chomp $prog; if ($prog eq ''){print "\nERROR: Cannot find Freebayes. Please install Freebayes in your path or include its installation directory at the top of get_SNPs.pl\n\n"; exit;}}
+if ($caller eq 'bcftools'){my $prog = `command -v ${bcftools}bcftools`; chomp $prog; if ($prog eq ''){print "\nERROR: Cannot find BCFtools. Please install BCFtools in your path or include its installation directory at the top of get_SNPs.pl\n\n"; exit;}}
 
 ## Timestamps and logs
 my $start = localtime();
@@ -144,7 +159,7 @@ if ($mh){
  }
 
 ## Running read mapping/SNP calling
-my $fasta = undef; my $fastq = undef; my $file = undef; my $fa = undef; my $dir; my $qdir;
+my $fasta = undef; my $fastq = undef; my $file = undef; my $fa = undef; my $dir; my $qdir; my $flagstat = undef;
 foreach (@fasta){ ## Creating indexes
 	$fasta = $_; ($fa, $dir) = fileparse($fasta);
 	if ($mapper eq 'bwa'){system "$bwa"."bwa index -a is $_";}
@@ -169,7 +184,10 @@ if (@fastq){ ## Single ends mode
 			if ($mapper eq 'bwa'){system "$bwa"."bwa $algo -t $threads $fasta $fastq -f $file.$fa.$mapper.sam 2>> mapping.$mapper.log";} ## Appending STDERR to mapping.$mapper.log"
 			elsif ($mapper eq 'bowtie2'){system "$bowtie2"."bowtie2 -p $threads -x $fa.bt2 -U $fastq -S $file.$fa.$mapper.sam 2>> mapping.$mapper.log";} 
 			elsif ($mapper eq 'hisat2'){system "$hisat2"."hisat2 -p $threads --phred33 -x $fa.ht -U $fastq --no-spliced-alignment -S $file.$fa.$mapper.sam 2>> mapping.$mapper.log";}
-			samtools(); variant(); stats(); logs(); ## Printing stats
+			samtools(); 
+			unless ($rmo){variant();} ## Calling variants
+			unless ($nostat){stats();} ## Printing stats
+			logs();
 			## Cleaning SAM/BAM files
 			unless ($bam) {system "rm $file.$fa.$mapper.bam";}
 			unless ($sam) {system "rm $file.$fa.$mapper.sam";}
@@ -193,7 +211,10 @@ if (@pe1 && @pe2){ ## Paired ends mode
 			if ($mapper eq 'bwa'){system "$bwa"."bwa $algo -t $threads $fasta $pe1 $pe2 -f $file.$fa.$mapper.sam 2>> mapping.$mapper.log";} ## Appending STDERR to mapping.$mapper.log"
 			elsif ($mapper eq 'bowtie2'){system "$bowtie2"."bowtie2 -p $threads -x $fa.bt2 -X $maxins -1 $pe1 -2 $pe2 -S $file.$fa.$mapper.sam 2>> mapping.$mapper.log";} 
 			elsif ($mapper eq 'hisat2'){system "$hisat2"."hisat2 -p $threads --phred33 -x $fa.ht -1 $pe1 -2 $pe2 --no-spliced-alignment -S $file.$fa.$mapper.sam 2>> mapping.$mapper.log";}
-			samtools(); variant(); stats(); logs(); ## Printing stats
+			samtools(); 
+			unless ($rmo){variant();} ## Calling variants
+			unless ($nostat){stats();} ## Printing stats
+			logs();
 			## Cleaning SAM/BAM files
 			unless ($bam) {system "rm $file.$fa.$mapper.bam";}
 			unless ($sam) {system "rm $file.$fa.$mapper.sam";}
@@ -205,10 +226,14 @@ if (@pe1 && @pe2){ ## Paired ends mode
 if ($mapper eq 'bwa'){system "mkdir $mapper.indexes; mv ${dir}*.amb ${dir}*.ann ${dir}*.bwt ${dir}*.fai ${dir}*.pac ${dir}*.sa $mapper.indexes/";}
 elsif ($mapper eq 'bowtie2'){system "mkdir $mapper.indexes; mv *.bt2 ${dir}*.fai $mapper.indexes/";}
 elsif ($mapper eq 'hisat2'){system "mkdir $mapper.indexes; mv *.ht2 ${dir}*.fai $mapper.indexes/";}
-system "mkdir $mapper.$caller.VCFs; mv *.vcf $mapper.$caller.VCFs/";
-system "mkdir $mapper.$caller.coverage; mv *.$mapper.coverage $mapper.$caller.coverage/";
-system "mkdir $mapper.$caller.depth; mv *.$mapper.depth $mapper.$caller.depth/";
-system "mkdir $mapper.$caller.stats; mv *.$mapper.$type.stats $mapper.$caller.stats/";
+if ($bam){system "mkdir $mapper.bam; mv *.bam $mapper.bam/";}
+if ($sam){system "mkdir $mapper.sam; mv *.bam $mapper.sam/";}
+unless ($rmo) {system "mkdir $mapper.$caller.VCFs; mv *.vcf $mapper.$caller.VCFs/";}
+unless ($nostat){
+	system "mkdir $mapper.$caller.coverage; mv *.$mapper.coverage $mapper.$caller.coverage/";
+	system "mkdir $mapper.$caller.depth; mv *.$mapper.depth $mapper.$caller.depth/";
+	system "mkdir $mapper.$caller.stats; mv *.$mapper.$type.stats $mapper.$caller.stats/";
+}
 
 ## Printing timestamps and logs
 my $end = localtime();
@@ -228,38 +253,50 @@ sub samtools{
 	system "$samtools"."samtools sort -@ $threads -o $file.$fa.$mapper.bam $file.$fa.bam";
 	system "$samtools"."samtools depth -aa $file.$fa.$mapper.bam > $file.$fa.$mapper.coverage"; ## Printing per base coverage information
 	system "rm $file.$fa.bam"; ## Discarding unsorted BAM file
+	$flagstat = `${samtools}samtools flagstat $file.$fa.$mapper.bam`;
 }
 
 sub variant{
-	print "Calling variants with $caller on $fasta...\n\n";
-	if ($caller eq 'varscan2'){
-		if (($type eq 'snp')||($type eq 'indel')){system "$samtools"."samtools mpileup -f $fasta $file.$fa.$mapper.bam | java -jar $varjar mpileup2$type --min-coverage $mc --min-reads2 $mr --min-avg-qual $maq --min-var-freq $mvf --min-freq-for-hom $mhom --p-value $pv --strand-filter $sf --output-vcf > $file.$fa.$mapper.$type.vcf";}
-		elsif ($type eq 'both'){system "$samtools"."samtools mpileup -f $fasta $file.$fa.$mapper.bam | java -jar $varjar mpileup2cns --min-coverage $mc --min-reads2 $mr --min-avg-qual $maq --min-var-freq $mvf --min-freq-for-hom $mhom --p-value $pv --strand-filter $sf --variants --output-vcf > $file.$fa.$mapper.$type.vcf";}
-		else {print "\nERROR: Unrecognized variant type. Please use: snp, indel, or both\n\n"; exit;}
+	(my $passQC) = ($flagstat =~ /(\d+)\s+\+\s+\d+ in total/s); print "\nQC-passed reads mapping to the genome = $passQC\n\n";
+	if ($passQC == 0){ ## Checking if BAM file is empty
+		print "No QC-passed reads mapped to the genome; skipping variants calling\n\n";
+		open VCF, ">$file.$fa.$mapper.$type.vcf"; print VCF '## No QC-passed reads mapped to the genome'."\n";
+		close VCF;
 	}
-	elsif ($caller eq 'bcftools'){
-		if ($type eq 'snp'){system "$samtools"."samtools mpileup -ugf $fasta $file.$fa.$mapper.bam | $bcftools"."bcftools call -vmO v -V indels --ploidy $ploidy --output $file.$fa.$mapper.$type.vcf";}
-		elsif ($type eq 'indel'){system "$samtools"."samtools mpileup -ugf $fasta $file.$fa.$mapper.bam | $bcftools"."bcftools call -vmO v -V snps --ploidy $ploidy --output $file.$fa.$mapper.$type.vcf";}
-		elsif ($type eq 'both'){system "$samtools"."samtools mpileup -ugf $fasta $file.$fa.$mapper.bam | $bcftools"."bcftools call -vmO v --ploidy $ploidy --output $file.$fa.$mapper.$type.vcf";}
-		else {print "\nERROR: Unrecognized variant type. Please use: snp, indel, or both\n\n"; exit;}
-	}
-	elsif ($caller eq 'freebayes'){ ## single thread only, parallel version behaving wonky
-		system "$samtools"."samtools index $file.$fa.$mapper.bam";
-		system "$freebayes"."freebayes -f $fasta -p $ploidy $file.$fa.$mapper.bam > $file.$fa.$mapper.$caller.SNP.vcf";
-		system "rm $file.$fa.$mapper.bam.bai";
+	else{ ## If not empty, proceed; empty BAM files create isssues when piping mpileup to some variant callers (e.g. VarScan2).
+		print "Calling variants with $caller on $fasta...\n\n";
+		if ($caller eq 'varscan2'){
+			if (($type eq 'snp')||($type eq 'indel')){system "$samtools"."samtools mpileup -f $fasta $file.$fa.$mapper.bam | java -jar $varjar mpileup2$type --min-coverage $mc --min-reads2 $mr --min-avg-qual $maq --min-var-freq $mvf --min-freq-for-hom $mhom --p-value $pv --strand-filter $sf --output-vcf > $file.$fa.$mapper.$type.vcf";}
+			elsif ($type eq 'both'){system "$samtools"."samtools mpileup -f $fasta $file.$fa.$mapper.bam | java -jar $varjar mpileup2cns --min-coverage $mc --min-reads2 $mr --min-avg-qual $maq --min-var-freq $mvf --min-freq-for-hom $mhom --p-value $pv --strand-filter $sf --variants --output-vcf > $file.$fa.$mapper.$type.vcf";}
+			else {print "\nERROR: Unrecognized variant type. Please use: snp, indel, or both\n\n"; exit;}
+		}
+		elsif ($caller eq 'bcftools'){
+			if ($type eq 'snp'){system "$samtools"."samtools mpileup -ugf $fasta $file.$fa.$mapper.bam | $bcftools"."bcftools call -vmO v -V indels --ploidy $ploidy --output $file.$fa.$mapper.$type.vcf";}
+			elsif ($type eq 'indel'){system "$samtools"."samtools mpileup -ugf $fasta $file.$fa.$mapper.bam | $bcftools"."bcftools call -vmO v -V snps --ploidy $ploidy --output $file.$fa.$mapper.$type.vcf";}
+			elsif ($type eq 'both'){system "$samtools"."samtools mpileup -ugf $fasta $file.$fa.$mapper.bam | $bcftools"."bcftools call -vmO v --ploidy $ploidy --output $file.$fa.$mapper.$type.vcf";}
+			else {print "\nERROR: Unrecognized variant type. Please use: snp, indel, or both\n\n"; exit;}
+		}
+		elsif ($caller eq 'freebayes'){ ## single thread only, parallel version behaving wonky
+			system "$samtools"."samtools index $file.$fa.$mapper.bam";
+			system "$freebayes"."freebayes -f $fasta -p $ploidy $file.$fa.$mapper.bam > $file.$fa.$mapper.$type.vcf";
+			system "rm $file.$fa.$mapper.bam.bai";
+		}
 	}
 }
 
 sub stats{
+	my $run_time = time - $tstart; my $mend = localtime();
 	print "\nCalculating stats...\n";
 	open COV, "<$file.$fa.$mapper.coverage";
-	open SN, "<$file.$fa.$mapper.$type.vcf";
+	unless ($rmo){open SN, "<$file.$fa.$mapper.$type.vcf";}
 	open STATS, ">$file.$fa.$mapper.$type.stats";
 	open DEPTH, ">$file.$fa.$mapper.depth"; print DEPTH "Contig\tAverage depth\tAverage (all contigs)\tDifference\tRatio Contig\/Average\n";
 	my $total = 0; my $covered = 0; my $nocov = 0; my $max = 0; my $sumcov; my $sn =0;
-	while (my $line = <SN>){
-		if ($line =~ /^#/){next;}
-		else {$sn++;}
+	unless ($rmo){
+		while (my $line = <SN>){
+			if ($line =~ /^#/){next;}
+			else {$sn++;}
+		}
 	}
 	my %data = (); my @contigs = ();
 	while (my $line = <COV>){
@@ -289,7 +326,9 @@ sub stats{
 		my $average = ($data{$tmp}[1]/$data{$tmp}[0]);
 		$average = sprintf("%.2f", $average);
 		my $diff = $average - $avc; $diff = sprintf("%.2f", $diff);
-		my $ratio = $average/$avc; $ratio = sprintf("%.2f", $ratio);
+		my $ratio;
+		if ($avc > 0){$ratio = $average/$avc; $ratio = sprintf("%.2f", $ratio);} ## Preventing possible division by zero
+		else{$ratio = 0;}
 		print DEPTH "$tmp\t$average\t$avc\t$diff\t$ratio\n";
 	} 
 	print STATS "FASTQ file(s) used: $file (and mate, if PE)\n";
@@ -299,13 +338,16 @@ sub stats{
 	print STATS "Average sequencing depth\t$avc"."X\n";
 	if ($total == $covered){print STATS "Sequencing breadth (percentage of bases covered by at least one read)\t100%\n";}
 	if ($total != $covered){my $av_cov = sprintf("%.2f%%", ($covered/$total)*100); print STATS "Sequencing breadth (percentage of bases covered by at least one read)\t$av_cov\n";}
-	my $snkb = sprintf("%.2f", ($sn/$covered)*1000);
-	if ($type eq 'both'){print STATS "Total number of SNPs + indels found: $sn\n"; print STATS "Average number of SNPs + indels per Kb: $snkb\n";}
-	elsif ($type eq 'snp') {print STATS "Total number of SNPs found: $sn\n"; print STATS "Average number of SNPs per Kb: $snkb\n";}
-	elsif ($type eq 'indel') {print STATS "Total number of indels found: $sn\n"; print STATS "Average number of indels per Kb: $snkb\n";}
+	unless ($rmo){
+		my $snkb = sprintf("%.2f", ($sn/$covered)*1000);
+		if ($type eq 'both'){print STATS "Total number of SNPs + indels found: $sn\n"; print STATS "Average number of SNPs + indels per Kb: $snkb\n";}
+		elsif ($type eq 'snp') {print STATS "Total number of SNPs found: $sn\n"; print STATS "Average number of SNPs per Kb: $snkb\n";}
+		elsif ($type eq 'indel') {print STATS "Total number of indels found: $sn\n"; print STATS "Average number of indels per Kb: $snkb\n";}
+	}
 	print STATS "\n## SAMTOOLS flagstat metrics\n";
+	print STATS "$flagstat\n";
 	close STATS;
-	system "$samtools"."samtools flagstat $file.$fa.$mapper.bam >> $file.$fa.$mapper.$type.stats";
+	print "Time to calculate stats: $run_time seconds\n";
 }
 
 sub logs{
@@ -322,19 +364,25 @@ sub print_options{
 	print MAP "Read mapper: $mapper\n";
 	if ($mapper eq 'bwa'){print MAP "BWA alogrithm: $algo\n";}
 	if ($mapper eq 'bowtie'){print MAP "Max insert size for bowtie: $maxins nt\n";}
-	print MAP "Variant caller: $caller\n";
-	if ($caller eq 'varscan2'){
-		print MAP "   Varscan jar file used: $varjar\n";
-		print MAP "   Minimum read depth at a position to make a call: $mc\n";
-		print MAP "   Minimum supporting reads at a position to call variants: $mr\n";
-		print MAP "   Minimum base quality at a position to count a read: $maq\n";
-		print MAP "   Minimum variant allele frequency threshold: $mvf\n";
-		print MAP "   Minimum frequency to call homozygote: $mhom\n";
-		print MAP "   P-value threshold for calling variants: $pv\n";
-		print MAP "   Strand filter: $sf\t\#\# 1 ignores variants with >90% support on one strand\n";
+	if ($rmo){print MAP "Read-mapping only requested, skipping variant calling.\n";}
+	if ($bam){print MAP "Keeping BAM files are requested.\n";}
+	if ($sam){print MAP "Keeping SAM files are requested.\n";}
+	if ($nostat){print MAP "Skipping stats calculations.\n";}
+	unless ($rmo){
+		print MAP "Variant caller: $caller\n";
+		if ($caller eq 'varscan2'){
+			print MAP "   Varscan jar file used: $varjar\n";
+			print MAP "   Minimum read depth at a position to make a call: $mc\n";
+			print MAP "   Minimum supporting reads at a position to call variants: $mr\n";
+			print MAP "   Minimum base quality at a position to count a read: $maq\n";
+			print MAP "   Minimum variant allele frequency threshold: $mvf\n";
+			print MAP "   Minimum frequency to call homozygote: $mhom\n";
+			print MAP "   P-value threshold for calling variants: $pv\n";
+			print MAP "   Strand filter: $sf\t\#\# 1 ignores variants with >90% support on one strand\n";
+		}
+		if ($caller eq "bcftools|freebayes"){print MAP "Setting ploidy to: $ploidy\n";}
+		if ($type eq 'snp'){print MAP "Searching for SNPs...\n";}
+		elsif ($type eq 'indel') {print MAP "Searching for indels...\n";}
+		elsif ($type eq 'both') {print MAP "Searching for SNPs and indels...\n";}
 	}
-	if ($caller eq "bcftools|freebayes"){print MAP "Setting ploidy to: $ploidy\n";}
-	if ($type eq 'snp'){print MAP "Searching for SNPs...\n";}
-	elsif ($type eq 'indel') {print MAP "Searching for indels...\n";}
-	elsif ($type eq 'both') {print MAP "Searching for SNPs and indels...\n";}
 }
