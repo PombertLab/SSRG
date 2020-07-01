ad9f249860fda650548bc362380d2b886246892b
Jean-Francois Pombert <jpombert@iit.edu>
2020-07-01 13:21:49 -0500

    Deleted the sliding windows options
    The random selection process is a lot more biologically relevant. The sliding option was no longer useful and thus was removed to reduce unessary complexity.

--- SSRG/SSRG.pl
+++ SSRG/SSRG.pl
@@ -1,61 +1,52 @@
 #!/usr/bin/perl
-## SSRG: Synthetic Short Read Generator; generates synthetic short reads in Fastq (Q33) format from multifasta files
 ## Pombert Lab, Illinois Tech (2015-2020)
-my $version = '1.6';
+my $version = '1.7';
 my $name = 'SSRG.pl';
 
-use strict; use warnings; use Getopt::Long qw(GetOptions);    
+use strict; use warnings; use Getopt::Long qw(GetOptions);
 
 my $options = <<"END_OPTIONS";
-
 NAME		$name
 VERSION		$version
-SYNOPSIS	Generates synthetic short reads in Fastq (Q33) format from multifasta files
+SYNOPSIS	SSRG: Synthetic Short Read Generator: 
+		Generates short reads in FASTQ (Q33) format from multifasta files
 
-EXAMPLE (simple): $name -f *.fasta
-EXAMPLE (advanced): $name -f *.fasta -r 250 -i 350 -s 20 -m rand -c 25 
+COMMAND		$name -f *.fasta -r 250 -i 350 -s 20 -c 25 -t PE
 
-OPTIONS: 
- -v (--version)		Display SSRG.pl version number
- -f (--fasta)		Fasta/multifasta input file(s)
- -l (--list)		List of fasta input file(s), one per line
- -r (--readsize)	Synthetic reads size [default: 100]
- -m (--mode)		Sliding windows or random selection (slide or rand) [default: rand]
- -t (--type)		Single or paired ends (se or pe) [default: pe]
- -i (--insert)		Insert size for pe [default: 250]
- -s (--sdev)		Insert size standard deviation percentage [default: 10]
- -c (--coverage)	Set sequencing depth [default: 50]
+OPTIONS:
+ -f (--fasta)		Fasta/multifasta file(s)
+ -l (--list)		List of fasta file(s), one per line
+ -r (--readsize)	Desired read size [default: 150]
+ -t (--type)		Read type: single (SE) or paired ends (PE) [default: PE]
+ -i (--insert)		PE insert size [default: 350]
+ -s (--sdev)		PE insert size standard deviation (in percentage) [default: 10]
+ -c (--coverage)	Desired sequencing depth [default: 50]
  -qs (--qscore)		Quality score associated with each base [default: 30]
- -q64          		Used the old Illumina Q64 FastQ format instead of the default Q33 Sanger/Illumina 1.8+ encoding
-
+ -q64          		Use the old Illumina Q64 FastQ format instead of the default Q33 Sanger/Illumina 1.8+ encoding
 END_OPTIONS
-my $usage = "\nUSAGE = perl SSRG.pl [options] -f *.fasta\n\n$options"; die $usage unless(scalar@ARGV>=1);
+die "\n$options\n" unless(scalar@ARGV>=1);
 
 my $start = localtime(); my $tstart = time;
 
 ## Declare options
-my $ver;
 my @fasta;
 my $list;
-my $readsize = 100;
+my $readsize = 150;
 my $type = 'pe';
-my $insert = 250;
+my $insert = 350;
 my $sdev = 10;
-my $mode = 'rand';
 my $slide;
 my $cov = '50';
-my $qscore = 30;        
+my $qscore = 30;
 my $q64 = '';		## Default is false so that Q33 format is used
 my $date = `date`;
 open LOG, ">>SSRG.log"; ## Keep track of read mapper STDERR messages
-print LOG "$date";
-print LOG "COMMAND LINE:\nSSRG.pl @ARGV\n\n";
+print LOG "\# $date";
+print LOG "COMMAND LINE: $name @ARGV\n\n";
 GetOptions(
-	'v|version' => \$ver,
 	'f|fasta=s@{1,}' => \@fasta,
 	'l|list=s' =>	\$list,
-	'readsize|r=s' => \$readsize,
-	'm|mode=s' => \$mode,
+	'r|readsize=i' => \$readsize,
 	'i|insert=i' => \$insert,
 	's|sdev=i' => \$sdev,
 	't|type=s' => \$type,
@@ -64,10 +55,8 @@ GetOptions(
 	'c|coverage=s' => \$cov
 );
 
-if ($ver){die "SSRG.pl $version\n";}
 my $warning1 = "\nFatal Error: Read size must be an integer\n"; die $warning1 unless(int($readsize) == $readsize);
 my $warning2 = "\nFatal Error: Insert size \($insert\) is smaller than read size \($readsize\). Please use a larger insert size.\n\n"; if ($type eq 'pe'){die $warning2 if ($insert < $readsize);}
-if ($mode eq 'slide'){slidcov();}
 my %ASCII = ASCII(); my $offset; my $score; qscore(); ## ASCII offset for quality score
 
 ## Working on files
@@ -84,9 +73,11 @@ if ($list){
 		push (@fasta, $fasta);
 	}
 }
+
 while ($fasta = shift @fasta) {
 	open IN, "<", "$fasta" or die "cannot open $fasta";
 	$fasta =~ s/\.fasta$//; $fasta =~ s/\.fsa$//; $fasta =~ s/\.fa$//; $fasta =~ s/\.fna$//; ## Removing file extensions
+	$type = lc($type);
 	if($type eq 'se'){open SE, ">$fasta.$readsize.SE.fastq";}
 	elsif($type eq 'pe'){open R1, ">$fasta.$readsize.R1.fastq"; open R2, ">$fasta.$readsize.R2.fastq";}
 	stats();
@@ -94,7 +85,7 @@ while ($fasta = shift @fasta) {
 	my @contigs = ();	## Initializing list of contigs per fasta file
 	my %contigs = ();	## Initializing hash of contigs per fasta file: key = contig name; value = sequence
 	my $name = undef;	## Contig name to be memorized
-	
+
 	while (my $line = <IN>){	## Creating list of contigs and hash of sequences
 		chomp $line;
 		if ($line =~ /^>(.*)$/){
@@ -104,36 +95,14 @@ while ($fasta = shift @fasta) {
 		}
 		else{$contigs{$name} .= $line;}
 	}
-	
+
 	while (my $todo = shift@contigs){ ## Parse each contig in the forward and reverse directions 
 		chomp $todo;
 		my $len = length($contigs{$todo});
 		my $seq = $contigs{$todo};
 		my $rev = reverse($seq);	## get the reverse complement of the contig
 		$rev =~ tr/ATGCNRYSWKMBVDHatgcnryswkmbvdh/TACGNYRWSMKVBHDtacgnyrwsmkvbhd/;
-		if (($mode eq 'slide') && (($type eq 'se'))){
-			my $i = 0;
-			while($i <= ($len-$readsize)) { 
-				$read = substr($seq, $i, $readsize);
-				$rev_read = substr($rev, $i, $readsize);
-				$count++;
-				se();
-				$i += $slide;                       
-			}
-		}
-		elsif (($mode eq 'slide') && (($type eq 'pe'))){
-			my $i = 0;
-			my $gap = $insert - ($readsize*2);
-			my $dev = int($insert/$sdev);
-			while($i <= ($len-$insert-($dev/2))){
-				my $rand = int(rand($dev) - ($dev/2));
-				$pe1 = substr($seq, $i, $readsize); $pe2 = substr($seq, $i+$gap+$rand+$readsize, $readsize); $pe2 = reverse($pe2); $pe2 =~ tr/ATGCNRYSWKMBVDHatgcnryswkmbvdh/TACGNYRWSMKVBHDtacgnyrwsmkvbhd/;
-				$rev1 = substr($rev, $i, $readsize); $rev2 = substr($rev, $i+$gap+$rand+$readsize, $readsize); $rev2 = reverse($rev2); $rev2 =~ tr/ATGCNRYSWKMBVDHatgcnryswkmbvdh/TACGNYRWSMKVBHDtacgnyrwsmkvbhd/;
-				pe();
-				$i += $slide;                       
-			}
-		}
-		elsif (($mode eq 'rand') && ($type eq 'se')){
+		if ($type eq 'se'){
 			my $numreads = int(($len*$cov)/$readsize);	## Number of reads to achieve desired depth
 			my $stdreads = int($numreads/2);			## Number of reads per strand +/-
 			for (1..$stdreads){
@@ -145,7 +114,7 @@ while ($fasta = shift @fasta) {
 				se();
 			}
 		}
-		elsif (($mode eq 'rand') && ($type eq 'pe')){
+		elsif ($type eq 'pe'){
 			my $numreads = int(($len*$cov)/$readsize);	## Number of reads to achieve desired depth
 			my $stdreads = int($numreads/4);			## Number of reads per strand +/-
 			my $gap = $insert - ($readsize*2);
@@ -183,11 +152,6 @@ sub ASCII{	## If Q33 Q=0 is ASCII 33; if Q64 Q=0 is ASCII 64
 	97 => 'a', 98 => 'b', 99 => 'c', 100 => 'd', 101 => 'e', 102 => 'f', 103 => 'g', 104 => 'h',
 	105 => 'i', 106 => 'j');
 }
-sub slidcov{
-	$slide = int($readsize/$cov);	## Determined by coverage and read size. Truncate to integer value.
-	if($type eq 'pe'){$slide = int($slide/2);}
-	my $warning3 = "Fatal Error: Read size is too small\n"; die $warning2 unless($readsize >= 1 && $slide >= 1);
-}
 sub qscore{								## Take qscore, calculate appropriate offset, determine correct ASCII character for quality score
 	if ($q64){$offset = $qscore + 64;}	## Print "Using Q64 format\n";
 	else{$offset = $qscore + 33;}		## Print "Using Q33 format\n";
@@ -205,9 +169,6 @@ sub stats{
 	print "Type:\t\t";
 	if ($type eq 'se'){print "Single ends\n";}
 	elsif ($type eq 'pe'){print "Paired ends\n";}
-	if ($mode eq 'slide'){print "Mode:\t\tSliding windows\nSlide every:\t$slide nucleotides\n";}
-	elsif ($mode eq 'rand'){print "Mode:\t\tRandom selection\n";}
-	print "\n\n";
 }
 sub se{ ## Print SE FastQ output:
 	$count++;
