8b4ce4aaadbf5e09ca6447521bf84d74cd274478
Pombert-JF <jpombert@iit.edu>
2021-03-13 16:11:08 -0600

    debugging path

--- get_SNPs.pl
+++ get_SNPs.pl
@@ -377,37 +377,37 @@ if ($mapper =~ /bwa|bowtie2|hisat2|ngmlr/){
 	mkdir ("${outdir}/$mapper.indexes",0755);
 }
 if ($mapper eq 'bwa'){ 
-	system "mv ${outdir}${dir}*.amb ${outdir}${dir}*.ann ${outdir}${dir}*.bwt \\
-	${outdir}${dir}*.fai ${outdir}${dir}*.pac ${outdir}${dir}*.sa ${outdir}/$mapper.indexes/";
+	system "mv ${outdir}/${dir}*.amb ${outdir}/${dir}*.ann ${outdir}/${dir}*.bwt \\
+	${outdir}/${dir}*.fai ${outdir}/${dir}*.pac ${outdir}/${dir}*.sa ${outdir}/$mapper.indexes/";
 }
 elsif ($mapper eq 'bowtie2'){
-	system "mv ${outdir}*.bt2 ${outdir}${dir}*.fai ${outdir}/$mapper.indexes/";
+	system "mv ${outdir}/*.bt2 ${outdir}/${dir}*.fai ${outdir}/$mapper.indexes/";
 }
 elsif ($mapper eq 'hisat2'){
-	system "mv ${outdir}*.ht2 ${outdir}${dir}*.fai ${outdir}/$mapper.indexes/";
+	system "mv ${outdir}/*.ht2 ${outdir}/${dir}*.fai ${outdir}/$mapper.indexes/";
 }
 elsif ($mapper eq 'ngmlr'){
-	system "mv ${outdir}*.ngm ${outdir}/$mapper.indexes/";
+	system "mv ${outdir}/*.ngm ${outdir}/$mapper.indexes/";
 }
 if ($bam){
 	mkdir ("${outdir}/$mapper.BAM",0755);
-	system "mv ${outdir}*.bam ${outdir}/$mapper.BAM/; mv ${outdir}*.csi ${outdir}/$mapper.BAM/";
+	system "mv ${outdir}/*.bam ${outdir}/$mapper.BAM/; mv ${outdir}/*.csi ${outdir}/$mapper.BAM/";
 }
 if ($sam){
 	mkdir ("${outdir}/$mapper.SAM",0755);
-	system "mv ${outdir}*.sam ${outdir}/$mapper.SAM/";
+	system "mv ${outdir}/*.sam ${outdir}/$mapper.SAM/";
 }
 unless ($rmo) {
 	mkdir ("${outdir}/$mapper.$caller.VCFs",0755);
-	system "mv ${outdir}*.vcf ${outdir}/$mapper.$caller.VCFs/";}
+	system "mv ${outdir}/*.vcf ${outdir}/$mapper.$caller.VCFs/";}
 unless ($nostat){
 	mkdir ("${outdir}/$mapper.$caller.depth",0755);
 	mkdir ("${outdir}/$mapper.$caller.stats",0755);
-	system "mv ${outdir}*.$mapper.depth ${outdir}/$mapper.$caller.depth/";
-	system "mv ${outdir}*.$mapper.$type.stats ${outdir}/$mapper.$caller.stats/";
+	system "mv ${outdir}/*.$mapper.depth ${outdir}/$mapper.$caller.depth/";
+	system "mv ${outdir}/*.$mapper.$type.stats ${outdir}/$mapper.$caller.stats/";
 }
 mkdir ("${outdir}/$mapper.$caller.coverage",0755);
-system "mv ${outdir}*.$mapper.coverage ${outdir}/$mapper.$caller.coverage/";
+system "mv ${outdir}/*.$mapper.coverage ${outdir}/$mapper.$caller.coverage/";
 
 ## Printing timestamps and logs
 my $end = localtime();
