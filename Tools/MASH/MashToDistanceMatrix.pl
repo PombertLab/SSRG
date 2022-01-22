4e4356e49e539b79d26661012fc74d03bb903689
Jean-Francois Pombert <jpombert@iit.edu>
2022-01-22 08:54:21 -0600

    outdir fix

--- Tools/MASH/MashToDistanceMatrix.pl
+++ Tools/MASH/MashToDistanceMatrix.pl
@@ -1,10 +1,14 @@
 #!/usr/bin/perl
 ## Pombert lab, 2019
-my $version = '1.2';
+my $version = '1.3';
 my $name = 'MashToDistanceMatrix.pl';
-my $updated = '2021-03-14';
+my $updated = '2022-01-22';
 
-use strict; use warnings; use Getopt::Long qw(GetOptions); use File::Basename;
+use strict;
+use warnings;
+use Getopt::Long qw(GetOptions);
+use File::Basename;
+use File::Path qw(make_path);
 
 ## Defining options
 my $usage = <<"OPTIONS";
@@ -38,46 +42,96 @@ GetOptions(
 	'f|format=s' => \$format
 );
 
-## Reformatting Mash output to a CSV or TSV matrix
+## Outdir
+unless (-d $outdir) { 
+	make_path(
+		$outdir, { mode => 0755 }
+	) or die "Can't create $outdir: $!\n";
+}
+
+## Choosing CSV or TSV
 $format = lc($format);
 my $sep;
-if ($format eq 'csv'){$sep = ",";}
-elsif ($format eq 'tsv'){$sep = "\t";}
-else {die "Format $format in not recognized. Please specify csv or tsv...\n";}
+if ($format eq 'csv'){
+	$sep = ",";
+}
+elsif ($format eq 'tsv'){
+	$sep = "\t";
+}
+else {
+	die "Format $format in not recognized. Please specify csv or tsv...\n";
+}
+
+## Working on Mash input file
+## https://github.com/marbl/Mash/blob/master/doc/sphinx/tutorials.rst
+
+my %mash = ();
+my %dist = ();
+my @OTU = ();
 
 open IN, "<", "$mash" or die "Can't read file $mash: $!\n";
+while (my $line = <IN>){
+
+	chomp $line;
+	my @columns = split ("\t", $line); 
+
+	my $reference = fileparse($columns[0]);
+	$reference =~ s/.fasta$//;
+
+	my $query = fileparse($columns[1]);
+	$query =~ s/.fasta$//;
+	
+	my $mashdist = $columns[2];
+	my $pval = $columns[3];
+	
+	my ($numerator, $denominator) = $columns[4] =~ /(\d+)\/(\d+)/;
+	
+	my $similarity = $numerator/$denominator;
+
+	## Applying an alternate method to calculate genetic distances,
+	## i.e. 1 - proportion of kmers shared; e.g. 1 - 857/1000 = 0.143.
+	my $distance = 1 - $similarity;
+	$distance = sprintf("%.3f", $distance);
+	
+	unless (exists $mash{$reference}){
+		push (@OTU, $reference);
+	}
+
+	$mash{$reference}{$query} = $mashdist;
+	$dist{$reference}{$query} = $distance;
+
+}
+
+## Creating matrix header
 my $outfile = "${outdir}/$prefix.$format";
 my $alternate = "${outdir}/$prefix.alternatedist.$format";
 open MASH, ">", "$outfile" or die "Can't create file $outfile: $!\n";
 open CORR, ">", "$alternate" or die "Can't create file $outfile: $!\n";
+
 print MASH "OTU";
 print CORR "OTU";
-
-my %hash = (); my %dist = (); my @OTU = ();
-while (my $line = <IN>){
-	chomp $line;
-	my @columns = split ("\t", $line); ## https://github.com/marbl/Mash/blob/master/doc/sphinx/tutorials.rst
-	my $reference = fileparse($columns[0]);
-	my $query = fileparse($columns[1]);
-	my $mashdist = $columns[2];
-	my $pval = $columns[3];
-	my ($numerator, $denominator) = $columns[4] =~ /(\d+)\/(\d+)/;
-	my $similarity = $numerator/$denominator;
-	my $distance = 1 - $similarity; ## Applying an alternate method to calculate genetic distances, i.e. 1 - proportion of kmers shared; e.g. 1 - 857/1000 = 0.143.
-	if (exists $hash{$reference}){
-		$hash{$reference} .= "${sep}$mashdist";
-		$dist{$reference} .= "${sep}$distance";
-	}
-	else{
-		$hash{$reference} .= "${sep}$mashdist";
-		$dist{$reference} .= "${sep}$distance";
-		push (@OTU, $reference);
-	}
+for (0..$#OTU) {
+	my $sp = $OTU[$_];
+	print MASH "$sep"."$sp";
+	print CORR "$sep"."$sp";
 }
-foreach (@OTU) {my $sp = $_;  $sp =~ s/.fasta$//; print MASH "${sep}$sp"; print CORR "${sep}$sp";}
-print MASH "\n"; print CORR "\n";
-while (my $taxa = shift@OTU){
-	my $spp = $taxa; $spp =~ s/.fasta$//;
-	print MASH "$spp"."$hash{$taxa}\n";
-	print CORR "$spp"."$dist{$taxa}\n";
+print MASH "\n";
+print CORR "\n";
+
+## Creating the matrix core
+for (0..$#OTU){
+
+	my $spp = $OTU[$_];
+	print MASH "$spp";
+	print CORR "$spp";
+
+	for (0..$#OTU){
+		my $otu = $OTU[$_];
+		my $mvalue = $mash{$spp}{$otu};
+		my $dvalue = $dist{$spp}{$otu};
+		print MASH "$sep"."$mvalue";
+		print CORR "$sep"."$dvalue";
+	}
+	print MASH "\n";
+	print CORR "\n";
 }
